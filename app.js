735a4dbb8c396e111fba0bd3c2cb60d03d8240d3
Matias Dure <497232@student.saxion.nl>
2023-10-24 13:52:05 +0200

    new message route and form

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index').router;
 var usersRouter = require('./routes/users');
+const newMessageRouter = require("./routes/newMessage");
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use("/new", newMessageRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
